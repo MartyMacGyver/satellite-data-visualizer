3cdc04ede8788971b87c25b8a4a8055fe8ae776c
Martin Falatic <martin@falatic.com>
2015-10-16 01:45:18 -0700

    Py2/3 compatibility

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Python Satellite Data Visualizer
 
-A Python tool for visualizing satellite positions from using TLE (two-line element set) formatted data using matplotlib
+A Python 2.7+/3.4+ tool for visualizing satellite positions from using TLE (two-line element set) formatted data using matplotlib
 
 This project is a fork of the code provided on Reddit by /u/chknoodle_aggie in the thread https://www.reddit.com/r/Python/comments/3gwzjr/using_pyephem_i_just_plotted_every_tleinfo/
 
-TLE data is sourced from from http://www.tle.info/
+This project also includes elements of the Python 3 port of this code by pklaus as published at https://gist.github.com/pklaus/469e603b105905170992
+
+TLE data is sourced from http://www.tle.info/
 
 More about TLE at https://en.wikipedia.org/wiki/Two-line_element_set
 
-Note: this is developed and tested using Python on Windows, but as long as the same libraries are available in your OS this should work there as well.
+Note: This is developed and tested using Python 2.7 and 3.4 on Windows, but as long as the same libraries are available in your OS this should work there as well.
--- allsatsabovehorizon.py
+++ allsatsabovehorizon.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
 
 # Original Python 2 version by user /u/chknoodle_aggie 2015-08-14 as posted in
 # https://www.reddit.com/r/Python/comments/3gwzjr/using_pyephem_i_just_plotted_every_tleinfo/
+
+from __future__ import print_function   # PEP 3105: Make print a function
 
 import math
 import time
@@ -12,51 +15,58 @@ import sys
 import os.path
 import zipfile
 import webbrowser
-degree_sign= u'\N{DEGREE SIGN}'
- 
- 
+degree_sign= '\N{DEGREE SIGN}'
+
+try:
+    from urllib.request import urlopen, URLopener
+except ImportError:
+    from urllib import URLopener
+    from urllib2 import urlopen
+
+try:
+    input = raw_input
+except NameError:
+    pass
+
 #http://www.celestrak.com/NORAD/elements/
  
 #import tle data from NORAD if internet_on(), save as sat=ephem.readtle(...)-----
-import urllib
-import urllib2
 def internet_on():
     try:
-        response=urllib2.urlopen('http://google.com',timeout=1) #tries to contact Google
+        response=urlopen('http://google.com',timeout=1) #tries to contact Google
         return True
     except: pass
     return False
+
 if internet_on():
-    file=urllib.URLopener()
+    file=URLopener()
     file.retrieve('http://www.tle.info/data/ALL_TLE.ZIP','ALL_TLE.ZIP')
-    print 'ALL_TLE.ZIP updated.'
+    print('ALL_TLE.ZIP updated.')
 else:
-    print 'Connection unavailable. Using saved ALL_TLE.ZIP (' + time.ctime(os.path.getmtime('ALL_TLE.ZIP')) + ')'
+    print('Connection unavailable. Using saved ALL_TLE.ZIP (' + time.ctime(os.path.getmtime('ALL_TLE.ZIP')) + ')')
 content=[]
- 
-print 'Unzipping ALL_TLE.ZIP...'
+
+print('Unzipping ALL_TLE.ZIP...')
 zip = zipfile.ZipFile('ALL_TLE.ZIP')  
 zip.extractall('.') 
-print "Done unzipping"
- 
-     
+print('Done unzipping')
+
 #load TLE data into python array
 with open('ALL_TLE.TXT') as f:
     content=f.readlines()
-    print int(len(content)/3),'TLEs loaded from ALL_TLE.TXT.'
+    print(int(len(content)/3),'TLEs loaded from ALL_TLE.TXT.')
     for i in range(0,len(content)):
         content[i]=content[i].replace('\n','') #remove endlines
-         
-         
+
 #Set observer location
 home = ephem.Observer()
-locationKeyword = raw_input('Location keyword: ') #type first few letters of the location keywords included in the entries below
+locationKeyword = input('Location keyword: ') #type first few letters of the location keywords included in the entries below
 locationName = ''
- 
+
 # #--Observer() in Abilene, TX---------------
 if locationKeyword.lower() == 'abilene'[0:len(locationKeyword)].lower():
     locationName = 'Abilene, TX'
-    print 'Observer in Abilene, TX.'
+    print('Observer in Abilene, TX.')
     home = ephem.Observer()
     home.lon = '-99.74'   # +E
     home.lat = '32.45'      # +N
@@ -68,7 +78,7 @@ if locationKeyword.lower() == 'abilene'[0:len(locationKeyword)].lower():
 #--Observer() in College Station, TX---------------
 elif locationKeyword.lower() == 'college station'[0:len(locationKeyword)].lower():
     locationName = 'College Station, TX'
-    print 'Observer in College Station, TX.'
+    print('Observer in College Station, TX.')
     home = ephem.Observer()
     home.lon = '-96.314444'   # +E
     home.lat = '30.601389'     # +N
@@ -80,7 +90,7 @@ elif locationKeyword.lower() == 'college station'[0:len(locationKeyword)].lower(
 #--Observer() in Oxford, England---------------
 elif locationKeyword.lower() == 'oxford'[0:len(locationKeyword)].lower():
     locationName = 'Oxford, England'
-    print 'Observer in Oxford, England.'
+    print('Observer in Oxford, England.')
     home = ephem.Observer()
     home.lon = '-1.2578'   # +E
     home.lat = '51.7519'     # +N
@@ -92,7 +102,7 @@ elif locationKeyword.lower() == 'oxford'[0:len(locationKeyword)].lower():
 #--Observer() in Bristol, RI---------------
 elif locationKeyword.lower() == 'bristol'[0:len(locationKeyword)].lower():
     locationName = 'Bristol, RI'
-    print 'Observer in Bristol, RI.'
+    print('Observer in Bristol, RI.')
     home = ephem.Observer()
     home.lon = '-71.2686'   # +E
     home.lat = '41.6842'     # +N
@@ -102,9 +112,9 @@ elif locationKeyword.lower() == 'bristol'[0:len(locationKeyword)].lower():
 #---------------------------------
  
 else:
-    print 'Location keyword not found.'
+    print('Location keyword not found.')
     sys.exit()
-     
+
 #read in each tle entry and save to list
 savedsats=[]
 satnames=[]
@@ -114,18 +124,18 @@ while 3*i_name+2<=len(content):
     savedsats.append(ephem.readtle(content[3*i_name],content[3*i_name+1],content[3*i_name+2]))
     satnames.append(content[3*i_name])
     i_name+=1
- 
+
 fig=plt.figure()
- 
+
 t=time.time()
-print t
+print(t)
 while 1:
     fig.clf()
     home.date=datetime.utcnow()
- 
+
     theta_plot = []
     r_plot = []
-     
+
     #click on a satellite to print its TLE name to the console
     def onpick(event):
         global t
@@ -138,15 +148,15 @@ while 1:
         i=0
         while  i<len(savedsats) and (math.degrees(theta)!=math.degrees(savedsats[i].az) or math.cos(savedsats[i].alt)!=r):
             i+=1
-        print satnames[i],'az='+str(math.degrees(savedsats[i].az)),'alt='+str(math.degrees(savedsats[i].alt))
- 
-         
+        print(satnames[i],'az='+str(math.degrees(savedsats[i].az)),'alt='+str(math.degrees(savedsats[i].alt)))
+
+
     for sat in savedsats: #for each satellite in the savedsats list...
         sat.compute(home)
         if math.degrees(sat.alt) > 0.0:
             theta_plot.append(sat.az)
             r_plot.append(math.cos(sat.alt))
-                 
+
     #plot initialization and display
     ax = plt.subplot(111, polar=True)
     ax.set_theta_direction(-1) #clockwise

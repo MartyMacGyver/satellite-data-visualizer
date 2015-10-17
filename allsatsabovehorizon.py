5b2bd96e4ddcb3362b82c78890fa0a0565eaa945
Martin Falatic <martin@falatic.com>
2015-10-16 18:10:51 -0700

    Extensive rewrite; more data sources; colors!

--- allsatsabovehorizon.py
+++ allsatsabovehorizon.py
@@ -6,6 +6,10 @@ Python Satellite Data Visualizer
 
 Author: Martin Falatic, 2015-10-15
 
+More about TLE:
+https://en.wikipedia.org/wiki/Two-line_element_set
+http://spaceflight.nasa.gov/realdata/sightings/SSapplications/Post/JavaSSOP/SSOP_Help/tle_def.html
+
 Based on code by user /u/chknoodle_aggie 2015-08-14 as posted in
 https://www.reddit.com/r/Python/comments/3gwzjr/using_pyephem_i_just_plotted_every_tleinfo/
 """
@@ -33,32 +37,46 @@ try:
 except NameError:
     pass
 
-tleSources = {
-    'AUS-CITY all':
-        ('http://www.tle.info/data/ALL_TLE.ZIP',
-         'ALL_TLE.ZIP'),
-    'Celestrak visual':
-        ('http://www.celestrak.com/NORAD/elements/visual.txt',
-         'visual.txt'),
-    'Celestrak BREEZE-M R/B':
-        ('http://www.celestrak.com/NORAD/elements/2012-044.txt',
-         '2012-044.txt'),
-}
+DEBUG = True
 
-content = []
-for sourceName in sorted(tleSources):
-    (sourceUrl, sourceFile) = tleSources[sourceName]
+tleSources = [
+    {'name':  'McCant\'s classifieds',
+     'url':   'https://www.prismnet.com/~mmccants/tles/classfd.zip',
+     'file':  'classfd.zip',
+     'color': '#000000'},
+
+    {'name':  'AUS-CITY all',
+     'url':   'http://www.tle.info/data/ALL_TLE.ZIP',
+     'file':  'ALL_TLE.ZIP',
+     'color': '#ffffff'},
+
+    {'name':  'Celestrak visual',
+     'url':   'http://www.celestrak.com/NORAD/elements/visual.txt',
+     'file':  'visual.txt',
+     'color': '#00ff00'},
+
+    {'name':  'Celestrak BREEZE-M R/B',
+     'url':   'http://www.celestrak.com/NORAD/elements/2012-044.txt',
+     'file':  '2012-044.txt',
+     'color': '#ff0000'},
+    ]
+
+
+def readTLEfile(source):
+    sourceName = source['name']
+    sourceUrl = source['url']
+    sourceFile = source['file']
     if os.path.isfile(sourceFile):
-        print('Using saved TLE data {} ({})'.format(sourceName,
+        print('Using saved TLE data {} ({})'.format(sourceFile,
               time.ctime(os.path.getmtime(sourceFile))))
     else:
-        print('Retrieving TLE data from {}'.format(sourceName))
+        print('Retrieving TLE data from {}'.format(sourceUrl))
         file = URLopener()
         try:
             file.retrieve(sourceUrl, sourceFile)
         except:
             print("Error: Failed to get TLE data")
-            sys.exit(1)
+            return None
         else:
             print('{} updated'.format(sourceFile))
 
@@ -71,104 +89,157 @@ for sourceName in sorted(tleSources):
 
     with open(sourceFile) as f:
         tempContent = f.readlines()
-        print(int(len(tempContent) / 3),
-              'TLEs loaded from {}'.format(sourceFile))
         for i in range(0, len(tempContent)):
             tempContent[i] = tempContent[i].replace('\n', '')
+        print(int(len(tempContent) / 3),
+              'TLEs loaded from {}'.format(sourceFile))
 
-    content.extend(tempContent)
+    return tempContent
 
-print()
 
-# Set observer location
-defaultLocation = "Abilene, TX"
-# Pontianak, Indonesia and Quito, Ecuador are right on the equator
-locationKeyword = ''
-while not locationKeyword:
-    locationKeyword = input('Location keyword: ')
-    if not locationKeyword or locationKeyword.isspace():
-        locationKeyword = defaultLocation
-        print("Using default location {}".format(locationKeyword))
+def processTLEdata(tleSources):
+    ''' Process each TLE entry '''
+    sats = []
+    for source in tleSources:
+        print("Processing {}".format(source['name']))
+        tempContent = readTLEfile(source=source)
+        print()
+        if tempContent:
+            i_name = 0
+            while 3 * i_name + 2 <= len(tempContent):
+                rawTLEname = tempContent[3 * i_name + 0]
+                rawTLEdat1 = tempContent[3 * i_name + 1]
+                rawTLEdat2 = tempContent[3 * i_name + 2]
+                partsTLEdat1 = rawTLEdat1.split()
+                try:
+                    body = ephem.readtle(rawTLEname, rawTLEdat1, rawTLEdat2)
+                except ValueError:
+                    print("Error: line does not conform to tle format")
+                    print("       " + rawTLEname)
+                    print("       " + rawTLEdat1)
+                    print("       " + rawTLEdat2)
+                    print()
+                else:
+                    name = body.name
+                    number = partsTLEdat1[1]
+                    designator = partsTLEdat1[2]
+                    sats.append({'name': name,
+                                 'number': number,
+                                 'designator': designator,
+                                 'color': source['color'],
+                                 'body': body, })
+                    # print("{} {} {} {}".
+                    #     format(name, number, designator, body))
+                i_name += 1
+                # if i_name > 100:
+                #     break
+    return sats
 
-    g = geocoder.google(locationKeyword)
-    if g.status != 'OK':
-        print('Location "{}" not found'.format(locationKeyword))
-        locationKeyword = ''
 
-print()
-print(g.json)
-home = ephem.Observer()
-(latitude, longitude) = g.latlng
-elevation = geocoder.elevation(g.latlng).meters
-home.lat = str(latitude)    # +N
-home.lon = str(longitude)   # +E
-home.elevation = elevation  # 524.0  # meters
-print()
-print('{}N {}E, {}m'.
-      format(latitude, longitude, elevation))
-print('{}N {}E, {}m'.
-      format(home.lat, home.lon, home.elevation))
-print()
+def getLocation():
+    defaultLocation = "Abilene, TX"
+    # Pontianak, Indonesia and Quito, Ecuador are right on the equator
+    locationKeyword = ''
+    while not locationKeyword:
+        locationKeyword = input(
+            'Enter location (default="{}"): '.format(defaultLocation))
+        if not locationKeyword or locationKeyword.isspace():
+            locationKeyword = defaultLocation
+        g = geocoder.google(locationKeyword)
+        if g.status != 'OK':
+            print('Location not found: "{}"'.format(locationKeyword))
+            locationKeyword = ''
+        else:
+            print()
+            print('Location found: "{}"'.format(g.location))
+    if DEBUG:
+        print()
+        print(g.json)
+    return g
 
-# Read in each TLE entry and save to list
-savedsats = []
-satnames = []
-i_name = 0
-while 3 * i_name + 2 <= len(content):
-    # For each satellite in the content list...
-    savedsats.append(ephem.readtle(
-        content[3 * i_name],
-        content[3 * i_name + 1],
-        content[3 * i_name + 2]))
-    satnames.append(content[3 * i_name])
-    i_name += 1
 
-fig = plt.figure()
-t = time.time()
+if __name__ == "__main__":
+    print()
+    print('-'*79)
+    print("Python Satellite Data Visualizer")
+    print('-'*79)
+    print()
 
-while 1:
-    fig.clf()
-    home.date = datetime.utcnow()
+    savedsats = processTLEdata(tleSources)
+    g = getLocation()
+    print()
+    (latitude, longitude) = g.latlng
+    elevation = geocoder.elevation(g.latlng).meters
 
-    theta_plot = []
-    r_plot = []
+    home = ephem.Observer()
+    home.lat = str(latitude)    # +N
+    home.lon = str(longitude)   # +E
+    home.elevation = elevation  # meters
+    if DEBUG:
+        print('{}N {}E, {}m'.format(latitude, longitude, elevation))
+        print('{}N {}E, {}m'.format(home.lat, home.lon, home.elevation))
+        print()
 
-    def handle_close(event):
-        print("Close event received")
+    print('-'*79)
+    print()
 
-    # Click on a satellite to print its TLE name to the console
-    def onpick(event):
-        global t
-        if time.time() - t < 1.0:  # limits calls to 1 per second
-            return
-        t = time.time()
-        ind = event.ind
-        r = np.take(r_plot, ind)[0]
-        theta = np.take(theta_plot, ind)[0]
+    fig = plt.figure()
+    t = time.time()
+    while 1:
+        fig.clf()
+        home.date = datetime.utcnow()
+
+        theta_plot = []
+        r_plot = []
+        colors = []
+
+        def handle_close(event):
+            # This doesn't work well yet
+            # print("Close event received")
+            pass
+
+        # Click on a satellite to print its TLE name to the console
+        def onpick(event):
+            global t
+            if time.time() - t < 1.0:  # limits calls to 1 per second
+                return
+            t = time.time()
+            ind = event.ind
+            r = np.take(r_plot, ind)[0]
+            theta = np.take(theta_plot, ind)[0]
+            i = 0
+            while i < len(savedsats):
+                satdata = savedsats[i]
+                if (math.degrees(theta) == math.degrees(satdata['body'].az) and
+                        math.cos(satdata['body'].alt) == r):
+                    break
+                i += 1
+            print('name=' + satdata['body'].name,
+                  'az=' + str(math.degrees(satdata['body'].az)),
+                  'alt=' + str(math.degrees(satdata['body'].alt)))
+
         i = 0
-        while (i < len(savedsats) and
-               (math.degrees(theta) != math.degrees(savedsats[i].az) or
-                math.cos(savedsats[i].alt) != r)):
-            i += 1
-        print(satnames[i], 'az=' + str(math.degrees(savedsats[i].az)),
-              'alt=' + str(math.degrees(savedsats[i].alt)))
+        for satdata in savedsats:  # for each satellite in the savedsats list
+            try:
+                satdata['body'].compute(home)
+            except ValueError:
+                pass  # print("Date out of range")
+            if math.degrees(satdata['body'].alt) > 0.0:
+                theta_plot.append(satdata['body'].az)
+                r_plot.append(math.cos(satdata['body'].alt))
+                colors.append(satdata['color'])
+                i += 1
 
-    for sat in savedsats:  # for each satellite in the savedsats list...
-        sat.compute(home)
-        if math.degrees(sat.alt) > 0.0:
-            theta_plot.append(sat.az)
-            r_plot.append(math.cos(sat.alt))
-
-    # plot initialization and display
-    ax = plt.subplot(111, polar=True)
-    ax.set_theta_direction(-1)  # clockwise
-    ax.set_theta_offset(np.pi / 2)  # put 0 degrees (north) at top of plot
-    ax.yaxis.set_ticklabels([])  # hide radial tick labels
-    ax.grid(True)
-    title = str(home.date)
-    ax.set_title(title, va='bottom')
-    ax.scatter(theta_plot, r_plot, picker=True, c='y')
-    fig.canvas.mpl_connect('pick_event', onpick)
-    fig.canvas.mpl_connect('close_event', handle_close)
-    ax.set_rmax(1.0)
-    plt.pause(0.25)
+        # plot initialization and display
+        ax = plt.subplot(111, polar=True)
+        ax.set_theta_direction(-1)  # clockwise
+        ax.set_theta_offset(np.pi / 2)  # put 0 degrees (north) at top of plot
+        ax.yaxis.set_ticklabels([])  # hide radial tick labels
+        ax.grid(True)
+        title = str(home.date)
+        ax.set_title(title, va='bottom')
+        ax.scatter(theta_plot, r_plot, c=colors, alpha=0.5, picker=True)
+        fig.canvas.mpl_connect('pick_event', onpick)
+        fig.canvas.mpl_connect('close_event', handle_close)
+        ax.set_rmax(1.0)
+        plt.pause(0.20)
